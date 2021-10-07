3aebb3a1014feb0d87f6b9f54d0bfe7d139fb7a2
Sourav <rudrasisodiya786@gmail.com>
2021-10-07 22:43:34 +0530

    Initial commit

---
+++ src/webparts/reactTesting/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactTestingWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactTestingWebPartStrings' {
+  const strings: IReactTestingWebPartStrings;
+  export = strings;
+}
